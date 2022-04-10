11340af91112ace8632dc1a9d89aa14101a7ae17
Minh Đăng Đỗ <myclone235@gmail.com>
2022-04-11 02:51:23 +0700

    Bổ sung phần I của bài 4

--- NumPy/4_NumPy_Array_Indexing.py
+++ NumPy/4_NumPy_Array_Indexing.py
@@ -1,4 +1,20 @@
 # I. Truy cập phần tử của Array
+
+#   - Việc lập chi mục mảng cũng giống như vicẹ truy cập một phần tử của mảng
+#   - Có thể truy cập 1 phần tử của mảng bằng cách tham chiếu điến chỉ số index của nó.
+#   - Chỉ số trong Array của NumPy luôn bắt đầu bằng số 0, nghĩa là phần tử đầu tiên của 1 array luôn có chỉ số index = 0, và phần tử thứ 2 có index = 1
+
+from os import system
+import numpy as np
+
+system('cls')
+arr = np.array([1, 2, 3, 4])
+print(arr[0])
+print(arr[1])
+print(arr[2]+arr[3])
+
 # II. Truy cập mảng 2 chiều
+
 # III. Truy cập mảng 3 chiều
+
 # IV. Chỉ số âm (Negative Indexing)
