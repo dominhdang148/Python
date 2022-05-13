50224f1d0102f5e9ee995e041f65bb05f2538b1e
Minh Đăng Đỗ <myclone235@gmail.com>
2022-05-13 23:42:08 +0700

    Bổ sung Django

--- Dj_Learning/members/views.py
+++ Dj_Learning/members/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Members
 # Create your views here.
 def index(request):
-    template=loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers= Members.objects.all().values()
+    template=loader.get_template('index.html')
+    context={
+        'mymembers':mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+def add(request):
+    template=loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+def addrecord(request):
+    x=request.POST['first']
+    y=request.POST['last']
+    member=Members(firstName=x, lastName=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
+def delete(request, id):
+    member=Members.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('index'))
