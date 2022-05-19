92f5887462cbefa5b72fb4273e2ac4dfd10fbdb5
Minh Đăng Đỗ <myclone235@gmail.com>
2022-05-19 16:42:06 +0700

    Bổ sung chức năng gắn nhãn POS

--- NLP_DEMO/tokenizer/urls.py
+++ NLP_DEMO/tokenizer/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.index, name='index')
+    path('index/',views.index, name='index'),
+    path('index/add/',views.add, name="add"),
+    path('result',views.result,name='result')
 ]
--- NLP_DEMO/tokenizer/views.py
+++ NLP_DEMO/tokenizer/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Sentece
 import underthesea as uds
-
-
-def index(request):
-    template = loader.get_template('token.html')
-    result=uds.word_tokenize("Xin chào các bạn")
+import numpy as np
+text=""
+def result(request):
+    template=loader.get_template('result.html')
+    global text
+    kq=np.array(uds.pos_tag(text))
     context={
-        'y':result,
+        'kq':kq,
     }
     return HttpResponse(template.render(context,request))
 
- 
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({},request))
+
+def add(request):
+    global text
+    text=request.POST['sent']
+    return HttpResponseRedirect(reverse('result'))
+
+
+    
